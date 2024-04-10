f2a65b0e93fec297d70481382a64c9e62f5a40de
jaynopponep <noppone.p@gmail.com>
2024-04-10 14:27:07 -0400

    More to EC

--- Course Notes/EC335.scm
+++ Course Notes/EC335.scm
@@ -35,5 +35,56 @@
 ; lists are not allowed.
 ; This recursive call will terminate when there are no more 1s that we can pair the 2 with.
 ; Once this recursive call terminates, the iterative call will in turn continue looking for
-; more 2s and then do exactly the same thing over again. The iterative call itself will
-; 
+; more 2s and then do exactly the same thing over again. The iterative call itself will terminate
+; once there are no more elements to even iterate through.
+
+; Specifically within the design, the iterative function will keep track of the variable
+; LD-elements which will increment each time we have found a pair of 1s and 2s.
+; So once the recursive call that we call within the iterative function is done, we should have
+; an updated value for LD-elements for each iteration. At the same time that the iterative function
+; updates its LD-elements, it also updates n to be smaller, without the number "2" that we
+; have now finished dealing with in the recursive function.
+
+; This essentially suggests a very simple GI to our iterative function:
+; n = [NYP][AP]
+; where n is the LD-number given to us
+; The AP will not be used in our function since they are the values that will not
+; be needed after it has become AP; in other words, 2s that are already processed.
+; NYP includes all of the leftover 2s that we must iterate to next once we are done
+; with the current iteration. 
+; Since we'll keep track of NYP, we terminate the iterative function when:
+; = (quotient nyp 10) 0
+; Note that even if we had nyp = 2, it still cannot pair with the very first 1
+; that we have pre-remove-crusting since it will create an empty list. Alternatives
+; would be it being non-1 AND non-2, which also doesn't matter, so we terminate
+; pretty much right when nyp is a 1 digit number.
+; Another GI is that n = NYP * 10^[# of digits in AP] + AP also stands as a GI
+; in this iterative function. For example:
+; n = 13232
+; If we are at nyp = 1, n = 1 * 10^[4] + 3232 => n = 1*10000+3232 = 13232 = True
+
+; The biggest question in this problem is, if we find a pair of 1s and 2s, doesn't that
+; mean that there are multiple possible complements to that pair? For example:
+; given n = 112121212122, let's remove the crust: 1212121212
+; Our first pair we identify would create a complement: 121212 1212
+; We see the complement 121212 can actually be multiple LD-elements itself.
+; In this case, we could call the iterative function within this 121212 and this
+; would basically be a separate problem within itself. We make the program do the work for us.
+; However, since our LD-number can be given as any combination of digits, we are also able to get
+; 12121231212, which is also the same problem since the 3 doesn't matter, but with our previous logic,
+; it does not call the iterative function. This will have the complement: 1212123 and 1212.
+; We must perform the iterative operation onto the complement also since it has its pairs within
+; the complement, but we do not use the remove-crust function. This shows that we must use the
+; remove-crust function outside of the iterative function, and use it as a guard and called before
+; parameters are passed into an -iter function. We can also conclude that we must perform the
+; iterative function on the given complement anyways no matter if we identify a 2 or not. 
+
+
+
+
+
+
+
+
+
+
