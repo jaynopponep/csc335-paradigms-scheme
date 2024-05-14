d83376547ad73fe85e12422d87d7a1f51f8678f2
jaynopponep <noppone.p@gmail.com>
2024-05-14 15:45:47 -0400

    some work on (unmake) and (bracket-num)

--- Course Notes/Project/Project.scm
+++ Course Notes/Project/Project.scm
@@ -34,25 +34,32 @@
 (define (slice n)
   (quotient n 10))
 
+(define (unmake n search-1-len)
+  (let ((n-length (length n)))
+    (modulo n (expt 10 (- n-length search-1-len)))))
+
 (define (make-pairs n complement)
   (let   ((n-length (length n))
           (my-comp-length (length complement)))
     (cond ((zero? complement) (get-LD-elements n))
-          ((not (> my-comp-length 2)) (get-LD-elements (modulo n (expt 10 (- n-length my-comp-length)))))
-          (else (+ (search-pair-iter complement (quotient complement 100) (rmd complement) (rmd (quotient complement 100)) 0)
-                   (get-LD-elements (modulo n (expt 10 (- n-length my-comp-length)))))))))
+          ((not (> my-comp-length 2)) (cons complement (get-LD-elements (modulo n (expt 10 (- n-length my-comp-length))))))
+          (else (cons (search-pair-iter complement (quotient complement 100) (rmd complement) (rmd (quotient complement 100)) '())
+                      (get-LD-elements (modulo n (expt 10 (- n-length my-comp-length)))))))))
 
 (define (get-LD-elements n)
-  (search-pair-iter (remove-crust n) (quotient (remove-crust n) 100) (rmd (remove-crust n)) (rmd (quotient (remove-crust n) 100)) 1))
-(define (search-pair-iter n n-search-1 2ptr 1ptr LD-elements)
-  (cond((zero? (quotient n 100)) LD-elements)
-       ((zero? n-search-1) (search-pair-iter (slice n) (quotient (slice n) 100) (rmd (slice n)) (rmd (quotient (slice n) 100)) LD-elements))
-       ((not (two? 2ptr)) (search-pair-iter (slice n) (slice n-search-1) (rmd (slice n)) (rmd (slice n-search-1)) LD-elements))
-       ((not (one? 1ptr)) (search-pair-iter n (slice n-search-1) 2ptr (rmd (slice n-search-1)) LD-elements))
+  (search-pair-iter (remove-crust n) (quotient (remove-crust n) 100) (rmd (remove-crust n)) (rmd (quotient (remove-crust n) 100)) '()))
+(define (search-pair-iter n n-search-1 2ptr 1ptr result)
+  (cond((zero? (quotient n 100)) result)
+       ((zero? n-search-1) (search-pair-iter (slice n) (quotient (slice n) 100) (rmd (slice n)) (rmd (quotient (slice n) 100)) (cons (bracket-num n n-search-1) result)))
+       ((not (two? 2ptr)) (search-pair-iter (slice n) (slice n-search-1) (rmd (slice n)) (rmd (slice n-search-1)) result))
+       ((not (one? 1ptr)) (search-pair-iter n (slice n-search-1) 2ptr (rmd (slice n-search-1)) result))
        (else
-        (search-pair-iter n (slice n-search-1) 2ptr (rmd (slice n-search-1)) (+ LD-elements (make-pairs n (slice n-search-1)))))))
+        (search-pair-iter n (slice n-search-1) 2ptr (rmd (slice n-search-1)) (cons (bracket-num n-search-1 (unmake n (length n-search-1))) (cons (make-pairs n (slice n-search-1)) result))))))
 
+(define (bracket-num first second)
+  (cons (quotient first 10) (list (list (get-LD-elements (quotient second 10))))))
 
+(get-LD-elements 111222) ; 4
 ;; 2.  Write and prove correct a syntax checker for TLS-scheme, as specified in HW 11.
 
 ;; 3.  Write and prove correct an interpreter for TLS extended by let*.
