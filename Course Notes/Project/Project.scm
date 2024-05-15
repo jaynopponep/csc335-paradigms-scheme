55ae8ef1d1a03872cb01e11e23d5c8e665a67568
Abrar Habib <abrarhabib285@gmail.com>
2024-05-14 20:54:12 -0400

    Added the chatgpt code for question 2

--- Course Notes/Project/Project.scm
+++ Course Notes/Project/Project.scm
@@ -68,5 +68,90 @@
 ; desired results:
 ; (1122) (1(2)) ((12)) ((1)2)
 ;; 2.  Write and prove correct a syntax checker for TLS-scheme, as specified in HW 11.
+(define (every pred lst)
+  (cond
+    ((null? lst) #t) ; Base case: empty list, predicate holds for all elements
+    ((pred (car lst)) (every pred (cdr lst))) ; Check predicate for current element and recurse on the rest of the list
+    (else #f))) ; Predicate fails for at least one element
 
+(define (all-symbols? lst)
+  (every symbol? lst)) ; Check if all elements of the list are symbols
+
+(define (syntax-check expr)
+  (define bound-vars '()) ; List to track bound variables
+
+  ; Helper function to add a variable to the bound-vars list
+  (define (add-bound var)
+    (set! bound-vars (cons var bound-vars)))
+
+  ; Helper function to check if a variable is bound
+  (define (bound? var)
+    (memq var bound-vars))
+
+  (define (check-expr e)
+    (cond
+      ((not (list? e)) #f) ; Must be a list
+      ((null? e) #f) ; Empty list is invalid
+      ((not (symbol? (car e))) #f) ; First element must be a symbol
+      ((eq? (car e) 'define) ; Check for define syntax
+       (and (>= (length e) 3) ; At least (define var value)
+            (symbol? (cadr e))
+            (add-bound (cadr e)) ; Add the variable to bound-vars
+            (check-expr (caddr e)))) ; Check the body of the define
+      ((eq? (car e) 'lambda) ; Check for lambda syntax
+       (and (>= (length e) 3) ; At least (lambda (args) body)
+            (list? (cadr e)) ; Second element must be a list of arguments
+            (every symbol? (cadr e)) ; All arguments must be symbols
+            (let ((args (cadr e)))
+              (map add-bound args)) ; Add lambda arguments to bound-vars
+            (check-expr (caddr e)))) ; Check the body of the lambda
+      ((eq? (car e) 'if) ; Check for if syntax
+       (and (>= (length e) 4)
+            (check-expr (cadr e)) ; Check condition
+            (check-expr (caddr e)) ; Check true-branch
+            (check-expr (cadddr e)))) ; Check false-branch
+      ((symbol? (car e)) (bound? (car e))) ; Check if symbol is bound
+      (else #t))) ; If none of the above, assume valid expression
+
+  (check-expr expr)) ; Start checking from the top-level expression
+
+;; Test cases
+(define test1 '(define x 5))
+(define test2 '(lambda (x y) (+ x y)))
+(define test3 '(if (> x 0) x (- x)))
+(define test4 'atom?)
+(define test5 '(define y 10))
+(define test6 '(lambda (z) (* y z))) ; Unbound variable 'y'
+
+(display (syntax-check test1))
+(newline) ; Should return #t
+(display (syntax-check test2))
+(newline) ; Should return #t
+(display (syntax-check test3))
+(newline) ; Should return #f (variable 'x' is unbound)
+(display (syntax-check test4))
+(newline) ; Should return #t
+(display (syntax-check test5))
+(newline) ; Should return #t
+(display (syntax-check test6))
+(newline) ; Should return #f (variable 'y' is unbound)
+
+
+
+
+; (display (syntax-check '(define x 5))) ; Should return #t
+; (newline)
+; (display (syntax-check '(lambda (x y) (+ x y)))) ; Should return #t
+; (newline)
+; (display (syntax-check '(if (> x 0) x (- x)))) ; Should return #t
+; (newline)
+; (display (syntax-check 'atom?)) ; Should return #f
+; (newline)
+; (display (syntax-check '(define))) ; Should return #f (invalid syntax for define)
+; (newline)
+; (display (syntax-check '(lambda x (+ x x)))) ; Should return #f (missing parentheses around arguments)
+; (newline)
+; (display (syntax-check '(if (> x 0)))) ; Should return #f (missing true-branch and false-branch)
+; (newline)
+; (display (syntax-check '(lambda (x) (+ x x))))
 ;; 3.  Write and prove correct an interpreter for TLS extended by let*.
